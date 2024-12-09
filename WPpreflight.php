31a421cb9e63c68dae6d440d1063605a1b788117
marcocapote <marcocapote15@gmail.com>
2024-12-09 11:21:40 -0300

    inicio

---
+++ WPpreflight.php
@@ -0,0 +1,17 @@
+<?php 
+/**
+ * Plugin Name: WPpreflight
+ * Description: Um plugin para checagem de preflight.
+ * Version: 1.0
+ * Author: marcocapote
+ */
+
+
+ function WPpreflight_shortcode() {
+    // Obtém o valor da página a partir do parâmetro GET (ou usa "alteracao" como padrão)
+
+    ob_start();
+        include plugin_dir_path(__FILE__) . 'views/preflight.php';
+    return ob_get_clean();
+}
+add_shortcode('WPpreflight', 'WPpreflight_shortcode');
---
+++ views/preflight.php
@@ -0,0 +1,1 @@
+<h2>testes preflight</h2>
